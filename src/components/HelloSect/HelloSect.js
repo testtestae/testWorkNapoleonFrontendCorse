6bd84be1ab2b72c54659b76d6af8f805a07fb266
Your Name <you@example.com>
2020-02-21 00:23:24 +0500

    add colors and HelloSect

---
+++ src/components/HelloSect/HelloSect.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './HelloSect.sass';
+
+export default class HelloSect extends React.Component{
+    render(){
+        return(
+            <div className="HelloSect">
+                
+            </div>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './index.sass';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import HelloSect from './components/HelloSect/HelloSect'
+
+import {Dots} from 'react-preloaders';
+
+//import App from './App';
+//import * as serviceWorker from './serviceWorker';
+
+class App extends React.Component{
+    render(){
+        return(
+            <HelloSect />
+        )
+    }
+}
+
+ReactDOM.render(<div><Dots /><App /></div>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+//serviceWorker.unregister();
